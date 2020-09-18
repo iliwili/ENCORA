e91c93840632cc65e4720c39998b298b6d441900
Ilias El Makrini <ilias0258@gmail.com>
2020-09-18 13:16:18 +0200

    first commit

---
+++ week1/start_to_program/les1/python-oef/les1/ilias.py
@@ -0,0 +1,4 @@
+print('Naam: Ilias El Makrini')
+print('Job: jobstudent bij Webly en samenwerking met een andere student voor een eigen \'startup\'')
+print('Hobby: gamen (van alles, shooters tot MMO\'s), lezen, fitness')
+print('Cursussen: Software programmeur, Arabisch module 1')
