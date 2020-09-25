54b7ce1f43752ea730893fe11d490d5777f537c8
Ilias El Makrini <ilias0258@gmail.com>
2020-09-25 07:18:38 +0200

    fixed small fault

---
+++ week2/les3/les3.py
@@ -0,0 +1,39 @@
+naam = "Ilias El Makrini"
+hw = "Hello world"
+
+# print(len(naam))
+# print(naam)
+# print(naam[4])
+# print(naam[-3])
+# print(naam[2:7])
+
+
+# print("\n")
+# print(naam.split())
+# voornaam = naam[:5]
+# achternaam = naam[6:]
+# print(voornaam)
+# print(achternaam)
+
+# print(achternaam[::-4])
+# print(voornaam.count('k'))
+# print(naam.count('k'))
+# print(naam.find(' '))
+# print(naam.find('i', 3))
+
+
+zin = 'De kat krabt de krollen van de trap.'
+# print(zin.count('e'))
+# print(zin.count(' '))
+# print(zin.lower().count('de'))
+
+# index_van = zin.find('van')
+# print(zin.find('a', index_van))
+
+# NUMBERS
+getal = 57
+print(str(getal))
+
+float_getal = float(390.8) 
+print(int(float_getal))
+print(int(390.2))
