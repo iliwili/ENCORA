352fa836d4f1dfb06067acfc674caf470fedb566
ilias el makrini <ilias0258@gmail.com>
2020-10-02 15:31:41 +0200

    les 6 start to program

--- week3/start-to-program/les5/hoger_lager.py
+++ week3/start-to-program/les5/hoger_lager.py
@@ -6,8 +6,8 @@ print('\nOPDRACHT HOGER LAGER\n')
 
 te_raden = random.randint(1,100)
 gok = 0
-gegokt = 0
 i = 0
+
 print('Het antwoord is ' + str(te_raden))
 
 while not gok == te_raden:
@@ -17,11 +17,12 @@ while not gok == te_raden:
 
     i += 1
     try: 
-        gok = int(input("\nGok " + str(i) + "(" + str(gegokt) +  ") : "))
+        gok = int(input("\nGok " + str(i) + ": "))
     except: 
         print('\nFoute input, geef een getal in!')
         i -= 1
     
+
     if gok < 1 or gok > 100:
         print('getal moet tussen 1 en 100 zijn')
         i -= 1
@@ -32,5 +33,3 @@ while not gok == te_raden:
             print("Hoger, nog", 10-i, "beurten!")
         elif gok == te_raden:
             print("\nGoed gedaan, je hebt het in " + str(i) + (i == 1 and " beurt" or " beurten") + " geraden!\n")
-
-
